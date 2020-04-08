77a34c7bb978b892663c2ce5d91dc4c2970e4f6e
Jakub <jakubsz3wczyk@gmail.com>
2020-04-08 20:03:07 +0200

    Changed expected count type

--- ir_denon.py
+++ ir_denon.py
@@ -6,4 +6,4 @@ class IrDenon:
     @staticmethod
     def send(command_key, count):
         cmd = getattr(DenonKey, command_key.upper()).value
-        subprocess.call(["irsend", "send_once", "denon", cmd, "--count", count])
+        subprocess.call(["irsend", "send_once", "denon", cmd, "--count", str(count)])
