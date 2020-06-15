34ce13ccef7cb430f72491a0284d5e84b00e3b1a
jakusz1 <13085272+jakusz1@users.noreply.github.com>
2020-06-15 20:38:50 +0200

    New BtHelper

--- bt_helper.py
+++ bt_helper.py
@@ -1,21 +1,33 @@
 import subprocess
 
-HOMEKIT_COMMAND = "python3 -m homekit.put_characteristic -f /home/pi/controller.json -a {} -c {} {}".format
+from homekit.controller import Controller
+from common import singleton
+
 POWER = "1.15"
 SATURATION = "1.25"
 HUE = "1.24"
 
+@singleton
 class BtHelper:
-    @staticmethod
-    def set_power(name, status):
-        subprocess.Popen(HOMEKIT_COMMAND(name, POWER, status).split())
+    def __init__(self):
+        self.controller = Controller('hci0')
+        self.controller.load_data('/home/pi/controller.json')
 
-    @staticmethod
-    def set_color(name, saturation, hue):
-        subprocess.Popen(f"{HOMEKIT_COMMAND(name, SATURATION, saturation)} -c {HUE} {hue}".split())
+    def _set(self, alias, charac):
+        pairing = self.controller.get_pairings()[alias]
 
-    @classmethod
-    def set_ct(cls, name, temperature):
+        characteristics = [(int(c[0].split('.')[0]),  # the first part is the aid, must be int
+                            int(c[0].split('.')[1]),  # the second part is the iid, must be int
+                            c[1]) for c in charac]
+        results = pairing.put_characteristics(characteristics, do_conversion=True)
+
+    def set_power(self, name, status):
+        self._set(name, [(POWER, status)])
+
+    def set_color(self, name, saturation, hue):
+        self._set(name, [(SATURATION, saturation), (HUE, hue)])
+
+    def set_ct(self, name, temperature):
         if temperature >= 6000:
             saturation = 0
             hue = 180
@@ -31,4 +43,4 @@ class BtHelper:
         else:
             saturation = 100
             hue = 25
-        cls.set_color(name, saturation, hue)
+        self.set_color(name, saturation, hue)
