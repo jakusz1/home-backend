7bc7fcdb98a09ab3fa70a8cdfbb64f4fe92bc339
Jakub Szewczyk <jakubsz3wczyk@gmail.com>
2021-01-24 18:36:33 +0100

    parallel set_all_power

--- light_repository.py
+++ light_repository.py
@@ -26,16 +26,12 @@ class LightRepository:
         return {k: v.get_info() for k, v in self.lights.items()}
 
     def set_all_power(self, state):
-        # proc = []
-        # for light in self.lights.values():
-        #     if light.power_mode != state:
-        #         p = Process(target=light.set_all_power, args=(state,))
-        #         p.start()
-        #         proc.append(p)
-        # for p in proc:
-        #     p.join()
+        proc = []
         for light in self.lights.values():
             if light.power_mode != state:
-                light.set_power(state)
-                light.set_second_power(state)
+                p = Process(target=light.set_all_power, args=(state,))
+                p.start()
+                proc.append(p)
+        for p in proc:
+            p.join()
         return self.get_info()
