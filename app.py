742052a36504de401ff4faa6c26932bf1bd4aec5
Jakub Szewczyk <jakubsz3wczyk@gmail.com>
2021-01-24 23:13:21 +0100

    added missing return

--- app.py
+++ app.py
@@ -124,12 +124,13 @@ def action_light(light_name, action):
     except Exception as error:
         return Response(f'{{"error": "{repr(error)}", "traceback": "{format_exc()}"}}', status=500,
                         mimetype='application/json')
-    
+
 
 @app.route('/api/v2/scene/<scene>', methods=['POST'])
 def set_scene(scene):
     try:
         LightRepository().set_scene(scene)
+        return Response(json.dumps(LightRepository().get_info()), status=200, mimetype='application/json')
     except Exception as error:
         return Response(f'{{"error": "{repr(error)}", "traceback": "{format_exc()}"}}', status=500,
                         mimetype='application/json')
